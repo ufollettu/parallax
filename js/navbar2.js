514909b571c0ef658e614862bd619adf1b946387
ufollettu <pasquale.merolle@gmail.com>
2015-10-08 22:46:34 +0200

    scrolling menu addiction

--- js/navbar2.js
+++ js/navbar2.js
@@ -4,8 +4,8 @@ $(function() {
     var nav_container = $(".nav-container");
     var nav = $("nav");
 
-    var top_spacing = 15;
-    var waypoint_offset = 50;
+    var top_spacing = 0;
+    var waypoint_offset = 20;
 
     nav_container.waypoint({
         handler: function(direction) {
@@ -36,38 +36,51 @@ $(function() {
         }
     });
 
-    var sections = $("section");
-    var navigation_links = $("nav a");
-
-    sections.waypoint({
-        handler: function(direction) {
-
-            var active_section;
-            active_section = $(this);
-            if (direction === "up") active_section = active_section.prev();
-
-            var active_link = $('nav a[href="#' + active_section.attr("id") + '"]');
-            navigation_links.removeClass("selected");
-            active_link.addClass("selected");
-
-        },
-        offset: '25%'
-    });
-
-
-
-    navigation_links.click(function(event) {
-
-        $.scrollTo(
-            $(this).attr("href"), {
-                duration: 200,
-                offset: {
-                    'left': 0,
-                    'top': -0.15 * $(window).height()
-                }
+    // Cache selectors
+    var lastId,
+        topMenu = $("#top-menu"),
+        topMenuHeight = topMenu.outerHeight(),
+        // All list items
+        menuItems = topMenu.find("a"),
+        // Anchors corresponding to menu items
+        scrollItems = menuItems.map(function() {
+            var item = $($(this).attr("href"));
+            if (item.length) {
+                return item;
             }
-        );
+        });
+
+    // Bind click handler to menu items
+    // so we can get a fancy scroll animation
+    menuItems.click(function(e) {
+        var href = $(this).attr("href"),
+            offsetTop = href === "#slide1" ? 0 : $(href).offset().top - topMenuHeight + 1;
+        $('html, body').stop().animate({
+            scrollTop: offsetTop
+        }, 300);
+        e.preventDefault();
     });
 
+    // Bind to scroll
+    $(window).scroll(function() {
+        // Get container scroll position
+        var fromTop = $(this).scrollTop() + topMenuHeight;
 
+        // Get id of current scroll item
+        var cur = scrollItems.map(function() {
+            if ($(this).offset().top < fromTop)
+                return this;
+        });
+        // Get the id of the current element
+        cur = cur[cur.length - 1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+        if (lastId !== id) {
+            lastId = id;
+            // Set/remove active class
+            menuItems
+                .parent().removeClass("active")
+                .end().filter("[href=#" + id + "]").parent().addClass("active");
+        }
+    });
 });
